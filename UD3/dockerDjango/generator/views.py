6b6f46f585738cf81e73e03c823e4daba867832a
DanielGS <a23daniel@iessanclemente.net>
2024-11-07 13:13:18 +0100

    Django, comienzo con templates

--- UD3/dockerDjango/generator/views.py
+++ UD3/dockerDjango/generator/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Ola buenas")
+    return render(request, 'generator/home.html', {'password':'hui45uhdf'})
+
+def password(request):
+    return render(request, 'generator/password.html')
