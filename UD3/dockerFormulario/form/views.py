e494658de044f532e965572df0736a236b12da40
DanielGS <a23daniel@iessanclemente.net>
2024-11-14 13:09:35 +0100

    Mucho commit

--- UD3/dockerFormulario/form/views.py
+++ UD3/dockerFormulario/form/views.py
@@ -7,4 +7,33 @@ def home(request):
     return render(request, 'form/home.html',)
 
 def answers(request):
-    return render(request, 'form/answers.html')
+    username = request.GET.get('username')
+    password = request.GET.get('password')
+    city = request.GET.get('city')
+    server = request.GET.get('server')
+    rol = request.GET.get('rol')
+
+    mail = ''
+    if(request.GET.get('mail')):
+        mail = 'Mail'
+
+    payroll = ''
+    if(request.GET.get('payroll')):
+        payroll = 'Payroll'
+
+    selfservice = ''
+    if(request.GET.get('selfservice')):
+        selfservice = 'Selfservice'
+
+    datos = {
+        "username": username,
+        "password": password,
+        "city":city,
+        "server":server,
+        "rol":rol,
+        "mail":mail,
+        "payroll":payroll,
+        "selfservice":selfservice,
+    }
+
+    return render(request, 'form/answers.html', datos)
