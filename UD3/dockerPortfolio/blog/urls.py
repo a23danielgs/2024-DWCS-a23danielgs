00892ed8a1ed64f6eb5447d0cc21aa3b3db72cf5
DanielGS <a23daniel@iessanclemente.net>
2024-11-15 12:52:40 +0100

    Blog details

--- UD3/dockerPortfolio/blog/urls.py
+++ UD3/dockerPortfolio/blog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path # type: ignore
 from . import views
 
 
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
+    path('<int:blog_id>/', views.detail, name='detail')
 ]
