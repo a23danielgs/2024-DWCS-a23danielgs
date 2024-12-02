f7f19c12a0b7e5d8e6edb5ca901a80b9094f53c8
a23danielgs <a23danielgs@iessanclemente.net>
2024-12-02 09:50:31 +0000

    Proyecto en la red

--- UD3/DjangoReview/DjangoProject/settings.py
+++ UD3/DjangoReview/DjangoProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3yav@ffx$%_c#cuoe+jpd1-9!phf1(r3%s-%h&0y^!cm!!6^rv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['a23danielgs.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
