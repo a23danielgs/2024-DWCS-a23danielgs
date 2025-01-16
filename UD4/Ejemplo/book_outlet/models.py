9ef3495e770f9f05af979fec0a6e32c74c78b8d1
DanielGS <a23daniel@iessanclemente.net>
2025-01-16 13:30:10 +0100

    cambios en el proyecto de Book

--- UD4/Ejemplo/book_outlet/models.py
+++ UD4/Ejemplo/book_outlet/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -7,10 +9,17 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
-        validators=[MinValueValidator(1),MaxValueValidator(100)]
-    )
+        validators=[MinValueValidator(1), MaxValueValidator(10)])
     author = models.CharField(null=True,max_length=150)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="",null=False, db_index=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse("detail",args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.title},{self.author} ({self.rating},{self.is_bestselling})"
+        return f"{self.title}:{self.id} - {self.slug}, {self.author} ({self.rating},{self.is_bestselling})"
--- UD4/Ejemplo/book_outlet/views.py
+++ UD4/Ejemplo/book_outlet/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.db.models import Avg,Min
+
+from .models import Book
 
 # Create your views here.
+
+def index(request):
+    books = Book.objects.all().order_by("-rating")
+    num_books = books.count()
+    agregation = books.aggregate(Avg("rating"),Min("rating"))
+
+    return render(request,"book_outlet/index.html",{"books":books,"total_number_of_books":num_books,"agregation":agregation})
+
+def book_detail(request,slug):
+    try:
+        book = Book.objects.get(slug=slug)
+    except:
+        raise Http404()
+    
+    return render(request,"book_outlet/book_detail.html",{
+        "title":book.title,
+        "author":book.author,
+        "rating":book.rating,
+        "is_bestselling":book.is_bestselling
+    })
